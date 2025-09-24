32f4271d99007f6e3c10c8d98bd190acf2377290
Jerónimo Vargas <jerovargas@gmail.com>
2025-09-23 21:19:32 -0600

    style(ui): thinner heat bar so thumb overlaps slightly

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -22,7 +22,7 @@ export default function Slider({
   return (
     <div className="w-full">
       <div
-        className="h-5 w-full rounded-md"
+        className="h-3 w-full rounded-md"
         style={{
           background:
             "linear-gradient(90deg, #8bc34a 0%, #ffb74d 30%, #ff7043 60%, #d32f2f 85%, #8A1C16 100%)"
@@ -36,7 +36,7 @@ export default function Slider({
         value={value}
         aria-label={ariaLabel}
         onChange={(e) => onChange(Number(e.target.value))}
-        className="-mt-5 w-full appearance-none bg-transparent accent-red [&::-webkit-slider-runnable-track]:h-5 [&::-webkit-slider-thumb]:h-6 [&::-webkit-slider-thumb]:w-6 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:shadow [&::-webkit-slider-thumb]:border [&::-webkit-slider-thumb]:border-red"
+        className="-mt-6 w-full appearance-none bg-transparent accent-red [&::-webkit-slider-runnable-track]:h-3 [&::-webkit-slider-thumb]:h-6 [&::-webkit-slider-thumb]:w-6 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:shadow [&::-webkit-slider-thumb]:border [&::-webkit-slider-thumb]:border-red"
       />
       <div className="mt-2 select-none">
         <div className="flex justify-between text-[11px] font-medium text-gray-700 px-1">
